8fa8c497dbc11eb252fd4ed2aa7a6dae368e8312
bazraatamir <69583471+bazraatamir@users.noreply.github.com>
2025-12-24 11:21:21 +0800

    second commit

--- index.py
+++ index.py
@@ -16,10 +16,17 @@ BLACK = (0, 0, 0)
 
 class Snake:
     def __init__(self):
-        pass
+        self.body = [(100,100),(100+BLOCK_SIZE,100)]
+        self.direction = (BLOCK_SIZE,0)
+
 
     def move(self):
-        pass
+        head_x, head_y = self.body[0]
+        dx,dy = self.direction
+        self.body.insert(0,(head_x+dx,head_y+dy))
+        self.body.pop()
+
+        
 
     def grow(self):
         pass
@@ -28,7 +35,8 @@ class Snake:
         pass
 
     def draw(self, screen):
-        pass
+        for i in self.body:
+            pygame.draw.rect(screen,WHITE,pygame.Rect(i[0],i[1],BLOCK_SIZE,BLOCK_SIZE))
 
 
 
@@ -49,6 +57,8 @@ class Game:
        pygame.init()
        self.screen = pygame.display.set_mode((1280, 720))
        self.running = True
+       self.clock = pygame.time.Clock()
+       self.snake = Snake()
 
     def handle_events(self):
        pass
@@ -57,15 +67,25 @@ class Game:
        pass
 
     def update(self):
-        pass
+        self.snake.move()
+
+        pygame.display.flip()
 
     def draw(self):
-            pass
-
+        self.screen.fill(BLACK)
+        self.snake.draw(self.screen)
+        self.snake.move()
+        
+        
     def run(self):
         while self.running:
-           pass
+           self.clock.tick(FPS)
+           self.draw()
+           
+           self.update()
+           
 
+           
         pygame.quit()
         
 
